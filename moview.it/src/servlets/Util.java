a56b6db2c523ba28086f0472e6dcd7353ba40547
Orlando <onapoli96@gmail.com>
2020-10-06 17:12:17 +0200

    Gio

--- moview.it/src/servlets/Util.java
+++ moview.it/src/servlets/Util.java
@@ -22,30 +22,4 @@ public class Util {
         return db;
     }
  
-    public static boolean searchUserInDb(String loginId, String loginPwd) {
-        boolean user_found = false;
- 
-        // Get the mongodb connection
-        MongoDatabase db = getConnection();
- 
-        // Get the mongodb collection.
-        MongoCollection col = db.getCollection(db_collection_name);
- 
-        // Get the particular record from the mongodb collection        
-        List obj = new ArrayList();
-        obj.add(new BasicDBObject("id", loginId));
-        obj.add(new BasicDBObject("pwd", loginPwd));
- 
-        // Form a where query
-        BasicDBObject whereQuery = new BasicDBObject();
-        whereQuery.put("$and", obj);
-        System.out.println("Sql query is?= " + whereQuery.toString());
- 
-        FindIterable cursor = col.find(whereQuery);
-        for(Document doc : cursor) {
-            System.out.println("Found?= " + doc);
-            user_found = true;
-        }
-        return user_found;
-    }
 }
